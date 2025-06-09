8292f259838e858d356082b14e5615304fb30097
vansh potpose <vanshpotpose@gmail.com>
2025-06-09 14:32:50 +0530

    .

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import {useState} from "react";
 import { users, complaints } from "./mockData";
 import Navbar from "./components/Navbar";
 import HomePage from "./components/Hompage compts/HomePage";
+import QueryPage from "./components/QueryPage compts/QueryPage";
 
 export default function Home() {
   const [currentPage, setCurrentPage] = useState("Home");
@@ -18,6 +19,7 @@ export default function Home() {
 
           <div className="">
             {currentPage === "Home" && <HomePage />}
+            {currentPage === "Query" && <QueryPage />}
           </div>
      
     </div>
